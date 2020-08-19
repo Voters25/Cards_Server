1afd164f8a9d53af7cdcc4abf52aa00c3e801b52
daniil <shkolnikov.daniil25@gmail.com>
2020-08-19 17:00:49 +0300

    Remove router

--- App.js
+++ App.js
@@ -86,7 +86,7 @@ passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) { // НЕ ОТРАБАТЫВАЕТ!!!
+passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
         done(err, user);
     });
